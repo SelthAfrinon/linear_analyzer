fe5c22a29235458e8a0d43421999f803dc01f192
Martin Rivera <martinl.rivera@yahoo.com>
2017-06-25 16:52:47 -0400

    Added additional looping structures to request different inputs for input file and output file.

--- linear_interceptor.f90
+++ linear_interceptor.f90
@@ -12,123 +12,135 @@ program linear_interceptor
     1 format(1A6, 1f7.3, 1A21, 1f9.3, 1A21, 1f10.3)
     2 format(1A18, 1f7.3, 1A21, 1f9.3)
 
-    write(*,*) "Would you like to read from a file? (0 for no, 1 for yes): "
-    read *, file_read
+    do
+        write(*,*) "Welcome to the Linear Interceptor Program."
+        write(*,*) "Would you like to read from a file? (0 for no, 1 for yes, 2 to exit program): "
+        read *, file_read
 
-    if (file_read == 0) then
-        do
-            ! input parameters
-            write (*,*) "Enter first distance and angle (deg) seperated by a space."
-            read *, dist_1, ang_1
-
-            write (*,*) "Enter second distance and angle (deg) seperated by a space."
-            read *, dist_2, ang_2
-
-            write (*,*) "Enter time between measurements."
-            read *, time
-
+        if (file_read == 0) then
             do
-                print *, "Enter the known interceptor property."
-                print *, "(0 for intercept time, 1 for intercept angle, 2 for interceptor speed"
-                print *, "3 to enter new incoming projectile, 4 to exit): "
-                read *, known_type
+                ! input parameters
+                write (*,*) "Enter first distance and angle (deg) seperated by a space."
+                read *, dist_1, ang_1
 
-                ! convert given angles to radians
-                ang_1 = deg_to_rad(ang_1)
-                ang_2 = deg_to_rad(ang_2)
+                write (*,*) "Enter second distance and angle (deg) seperated by a space."
+                read *, dist_2, ang_2
 
-                call calc_incoming(ang_1, ang_2, x1, y1, dist_1, x2, y2, time, dist_2, m1, b, proj_dist, proj_speed, proj_angle)
+                write (*,*) "Enter time between measurements."
+                read *, time
 
-                if (known_type == 0) then
-                ! branch for known time to intercept
-                    print *, "Enter intercept time: "
-                    read *, e_time
-                    call calc_int_time(xi, yi, x1, y1, x2, y2, time, e_time, interceptor_angle, interceptor_speed)
-                    interceptor_angle = rad_to_deg(interceptor_angle)
+                do
+                    print *, "Enter the known interceptor property."
+                    print *, "(0 for intercept time, 1 for intercept angle, 2 for interceptor speed"
+                    print *, "3 to enter new incoming projectile, 4 to exit): "
+                    read *, known_type
 
-                else if (known_type == 1) then
-                ! branch for known interception angle
-                    print *, "Enter intercept angle: "
-                    read *, interceptor_angle
-                    interceptor_angle = deg_to_rad(interceptor_angle)
-                    call calc_int_angle(interceptor_angle, xi, yi, m1, m2, b, y1, y2, time, e_time, interceptor_speed)
+                    ! convert given angles to radians
+                    ang_1 = deg_to_rad(ang_1)
+                    ang_2 = deg_to_rad(ang_2)
 
-                else if (known_type == 2) then
-                ! branch for known interceptor speed
-                    print *, "Enter interceptor speed: "
-                    read *, interceptor_speed
-                    call calc_int_speed(x1, x2, xi, y1, y2, yi, dx1, dy1,&
-                        interceptor_speed, a, d, c, sub_1, e_time, time, interceptor_angle)
-                    interceptor_angle = rad_to_deg(interceptor_angle)
+                    call calc_incoming(ang_1, ang_2, x1, y1, dist_1, x2, y2, time, dist_2, m1, b, proj_dist, proj_speed, proj_angle)
 
-                else if (known_type == 3) then
-                    exit
-                else if (known_type == 4) then
+                    if (known_type == 0) then
+                    ! branch for known time to intercept
+                        print *, "Enter intercept time: "
+                        read *, e_time
+                        call calc_int_time(xi, yi, x1, y1, x2, y2, time, e_time, interceptor_angle, interceptor_speed)
+                        interceptor_angle = rad_to_deg(interceptor_angle)
+
+                    else if (known_type == 1) then
+                    ! branch for known interception angle
+                        print *, "Enter intercept angle: "
+                        read *, interceptor_angle
+                        interceptor_angle = deg_to_rad(interceptor_angle)
+                        call calc_int_angle(interceptor_angle, xi, yi, m1, m2, b, y1, y2, time, e_time, interceptor_speed)
+
+                    else if (known_type == 2) then
+                    ! branch for known interceptor speed
+                        print *, "Enter interceptor speed: "
+                        read *, interceptor_speed
+                        call calc_int_speed(x1, x2, xi, y1, y2, yi, dx1, dy1,&
+                            interceptor_speed, a, d, c, sub_1, e_time, time, interceptor_angle)
+                        interceptor_angle = rad_to_deg(interceptor_angle)
+
+                    else if (known_type == 3) then
+                        exit
+                    else if (known_type == 4) then
+                        exit
+                    end if
+
+                    write(*,2) "Projectile Speed: ", proj_speed, "   Projectile Angle: ", rad_to_deg(proj_angle)
+                    write(*,1) "Time: ", e_time, "   Interceptor Speed: ", interceptor_speed,&
+                         "    Interceptor Angle: ", interceptor_angle
+
+                end do
+                if (known_type == 4) then
                     exit
                 end if
-
-                write(*,2) "Projectile Speed: ", proj_speed, "   Projectile Angle: ", rad_to_deg(proj_angle)
-                write(*,1) "Time: ", e_time, "   Interceptor Speed: ", interceptor_speed,&
-                     "    Interceptor Angle: ", interceptor_angle
-
             end do
-            if (known_type == 4) then
-                exit
-            end if
-        end do
+        else if (file_read == 1) then
+            do
+                write(*,*) "Please enter input file name/path: "
+                read *, file_name
+                open(10, file = file_name)
 
-    else if (file_read == 1) then
-        write(*,*) "Please enter input file name/path: "
-        read *, file_name
-        open(10, file = file_name)
+                write(*,*) "Please enter output file name/path: "
+                read *, file_name
+                open(11, file = file_name)
+                write(11,*) " PS     PA      TTI    IS     IA"
 
-        write(*,*) "Please enter output file name/path: "
-        read *, file_name
-        open(11, file = file_name)
-        write(11,*) " PS     PA      TTI    IS     IA"
+                do
+                    read(10,*, IOSTAT = io) dist_1, ang_1, dist_2, ang_2, time, known_type, dummy
+                    if(io < 0) then
+                        exit
+                    end if
+                    ang_1 = deg_to_rad(ang_1)
+                    ang_2 = deg_to_rad(ang_2)
+                    call calc_incoming(ang_1, ang_2, x1, y1, dist_1, x2, y2, time, dist_2, m1, b, proj_dist, proj_speed, proj_angle)
 
-        do
-            read(10,*, IOSTAT = io) dist_1, ang_1, dist_2, ang_2, time, known_type, dummy
-            if(io < 0) then
-                exit
-            end if
-            ang_1 = deg_to_rad(ang_1)
-            ang_2 = deg_to_rad(ang_2)
-            call calc_incoming(ang_1, ang_2, x1, y1, dist_1, x2, y2, time, dist_2, m1, b, proj_dist, proj_speed, proj_angle)
+                    if (known_type == 0) then
+                    ! branch for known time to intercept
+                        e_time = dummy
+                        call calc_int_time(xi, yi, x1, y1, x2, y2, time, e_time, interceptor_angle, interceptor_speed)
+                        interceptor_angle = rad_to_deg(interceptor_angle)
 
-            if (known_type == 0) then
-            ! branch for known time to intercept
-                e_time = dummy
-                call calc_int_time(xi, yi, x1, y1, x2, y2, time, e_time, interceptor_angle, interceptor_speed)
-                interceptor_angle = rad_to_deg(interceptor_angle)
+                    else if (known_type == 1) then
+                    ! branch for known interception angle
+                        interceptor_angle = dummy
+                        interceptor_angle = deg_to_rad(interceptor_angle)
+                        call calc_int_angle(interceptor_angle, xi, yi, m1, m2, b, y1, y2, time, e_time, interceptor_speed)
 
-            else if (known_type == 1) then
-            ! branch for known interception angle
-                interceptor_angle = dummy
-                interceptor_angle = deg_to_rad(interceptor_angle)
-                call calc_int_angle(interceptor_angle, xi, yi, m1, m2, b, y1, y2, time, e_time, interceptor_speed)
+                    else if (known_type == 2) then
+                    ! branch for known interceptor speed
+                        interceptor_speed = dummy
+                        call calc_int_speed(x1, x2, xi, y1, y2, yi, dx1, dy1,&
+                            interceptor_speed, a, d, c, sub_1, e_time, time, interceptor_angle)
+                        interceptor_angle = rad_to_deg(interceptor_angle)
+                    end if
+                    3 format(1f7.3, 1f8.3, 1f7.3, 1f7.3, 1f7.3)
+                    write(11,3) proj_speed, rad_to_deg(proj_angle), e_time, interceptor_speed, interceptor_angle
 
-            else if (known_type == 2) then
-            ! branch for known interceptor speed
-                interceptor_speed = dummy
-                call calc_int_speed(x1, x2, xi, y1, y2, yi, dx1, dy1,&
-                    interceptor_speed, a, d, c, sub_1, e_time, time, interceptor_angle)
-                interceptor_angle = rad_to_deg(interceptor_angle)
-            end if
-            3 format(1f7.3, 1f8.3, 1f7.3, 1f7.3, 1f7.3)
-            write(11,3) proj_speed, rad_to_deg(proj_angle), e_time, interceptor_speed, interceptor_angle
-
-        end do
-        write(11,*) ""
-        write(11,*) " Key:"
-        write(11,*) " PS = Incoming Projectile Speed"
-        write(11,*) " PA = Incoming Projectile Angle"
-        write(11,*) " TTI = Time To Intercept"
-        write(11,*) " IS = Interceptor Speed"
-        write(11,*) " IA = Interception Angle"
-        close(10)
-        close(11)
-    end if
+                end do
+                write(11,*) ""
+                write(11,*) " Key:"
+                write(11,*) " PS = Incoming Projectile Speed"
+                write(11,*) " PA = Incoming Projectile Angle"
+                write(11,*) " TTI = Time To Intercept"
+                write(11,*) " IS = Interceptor Speed"
+                write(11,*) " IA = Interception Angle"
+                close(10)
+                close(11)
+                write(*,*) file_name, " successfully written. Would you like to process another file? (0 for no, 1 for yes)"
+                read *, file_read
+                if(file_read == 0) then
+                    exit
+                end if
+            end do
+        else if (file_read == 2) then
+            exit
+        end if
+    end do
+    write(*,*) "Thank you for using the Linear Interceptor Program."
 end program linear_interceptor
 
 subroutine calc_incoming(ang_1, ang_2, x1, y1, dist_1, x2, y2, time, dist_2, m1, b, proj_dist, proj_speed, proj_angle)
